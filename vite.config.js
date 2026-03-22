5d0a2ffc628e40a0ac184c3635f7073599be71b2
rokey6950-cell <rokey6950@gmail.com>
2026-03-22 23:08:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          framer: ['framer-motion'],
+          recharts: ['recharts'],
+        }
+      }
+    }
   }
 })
